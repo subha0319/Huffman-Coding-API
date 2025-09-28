7a7d500b474fa0e7169380c8a2f2a4127c05c32a
subha0319 <subhashini0319@gmail.com>
2025-09-28 21:16:38 +0530

    Initial project setup

---
+++ backend/src/main/java/com/github/subha0319/huffman_api/HuffmanApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.subha0319.huffman_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuffmanApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HuffmanApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/github/subha0319/huffman_api/HuffmanApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.subha0319.huffman_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuffmanApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
